7fc48d885930d07e4ac88a60353ac80daa4dcd23
KodyReid <kody.reid7@gmail.com>
2021-05-14 12:19:39 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+// IIFE -- Immediately Invoked Function Expression
+"use strict";
+(function (){
+    
+function Start (){
+    
+    let myVariable = 20;
+    console.log("My Number Is: " + myVariable);
+       
+}
+
+window.addEventListener("load", Start)
+})();
+
+
+
+
